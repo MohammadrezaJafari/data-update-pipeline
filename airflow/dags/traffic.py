c385812d7eaf4983a227d4bfda57de4a1096f97c
Mreza <mine@Mines-MacBook-Pro.local>
2021-09-04 03:07:02 +0430

    reformat dag

--- airflow/dags/traffic.py
+++ airflow/dags/traffic.py
@@ -33,57 +33,57 @@ default_args = {
 dag = DAG(
     'traffic',
     default_args=default_args,
-    description='A simple tutorial DAG',
+    description='A Traffic Data Update Dag DAG',
     schedule_interval=timedelta(days=1),
 )
 
 # t1, t2 and t3 are examples of tasks created by instantiating operators
 generate_sauron = BashOperator(
     task_id='generate_sauron',
-    bash_command='date',
+    bash_command='echo "generate_sauron"',
     dag=dag,
 )
 
 prepare_sharedsreets_data = BashOperator(
     task_id='prepare_sharedsreets_data',
-    bash_command='date',
+    bash_command='echo "prepare_sharedsreets_data"',
     dag=dag,
 )
 
 prepare_sharedsreets_tiles = BashOperator(
     task_id='prepare_sharedsreets_tiles',
-    bash_command='date',
+    bash_command='echo "prepare_sharedsreets_tiles"',
     dag=dag,
 )
 
 prepare_sharedsreets_tiles_mmc = BashOperator(
     task_id='prepare_sharedsreets_tiles_mmc',
-    bash_command='date',
+    bash_command='echo "prepare_sharedsreets_tiles_mmc"',
     dag=dag,
 )
 
 prepare_valhalla_data = BashOperator(
     task_id='prepare_valhalla_data',
-    bash_command='date',
+    bash_command='echo "prepare_valhalla_data"',
     dag=dag,
 )
 
 deploy_valhalla_teh1 = BashOperator(
     task_id='deploy_valhalla_teh1',
-    bash_command='date',
+    bash_command='echo "deploy_valhalla_teh1"',
     dag=dag,
 )
 
 deploy_valhalla_teh2 = BashOperator(
     task_id='deploy_valhalla_teh2',
-    bash_command='date',
+    bash_command='echo "deploy_valhalla_teh2"',
     dag=dag,
 )
 
 generate_traffic_data = BashOperator(
     task_id='generate_traffic_data',
     depends_on_past=False,
-    bash_command='sleep 5',
+    bash_command='echo "generate_traffic_data"',
     retries=3,
     dag=dag,
 )
@@ -108,31 +108,22 @@ templated_command = """
 update_central_data_repository = BashOperator(
     task_id='update_central_data_repository',
     depends_on_past=False,
-    bash_command=templated_command,
-    params={'my_param': 'Parameter I passed in'},
+    bash_command='echo "update_central_data_repository"',
     dag=dag,
 )
 
 generate_cluster_label = BashOperator(
     task_id='generate_cluster_label',
     depends_on_past=False,
-    bash_command=templated_command,
-    params={'my_param': 'Parameter I passed in'},
+    bash_command='echo "generate_cluster_label"',
     dag=dag,
 )
 
-update_central_data_repository = BashOperator(
-    task_id='update_central_data_repository',
-    depends_on_past=False,
-    bash_command=templated_command,
-    params={'my_param': 'Parameter I passed in'},
-    dag=dag,
-)
 
 generate_sauron >> prepare_sharedsreets_data >> [prepare_sharedsreets_tiles, prepare_sharedsreets_tiles_mmc]
 
+prepare_sharedsreets_tiles >> generate_traffic_data >> update_central_data_repository
 prepare_sharedsreets_tiles >> prepare_valhalla_data >> [deploy_valhalla_teh1, deploy_valhalla_teh2]
 
-prepare_sharedsreets_tiles >> generate_traffic_data >> update_central_data_repository
 generate_cluster_label >> update_central_data_repository
 # prepare_sharedsreets_tiles >> generate_cluster_label >> generate_cluster_label
